8838dd4ed7741a815367cb580277e5c42b66cbb8
Susan29 <susansunny29@gmail.com>
2020-11-18 13:29:52 +0530

    c# console prj added

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            /*string message;
+            message = "Hello World";*/
+           // Console.WriteLine(message);
+            //Console.WriteLine($"My first program {message}");
+
+
+            /* string afriend;
+             afriend = "Susan";
+             Console.WriteLine($"Hello {afriend}");
+             Console.ReadLine();
+            */
+
+            /*string firstFriend = "Susan";
+            string secondFriend = "Divya";
+            Console.WriteLine($" My two friends are {firstFriend} and {secondFriend}");
+            Console.ReadLine();
+            */
+
+
+
+            string firstFriend = "Susan";
+            string secondFriend = "Rosy";
+            Console.WriteLine($"The no of characters in {firstFriend} is {firstFriend.Length}.");
+            Console.WriteLine($"The no of characters in {secondFriend} is {secondFriend.Length}.");
+            Console.ReadLine();
+
+        }
+
+    }
+}
